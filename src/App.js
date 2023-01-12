49458e937823f931bf1395bda611143e406f320b
mustafadurmaz <mdmz_001@hotmail.com>
2023-01-12 23:43:40 +0300

    Keycloak settings edit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./Keycloak"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import WelcomePage from "./pages/Homepage";
 import SecuredPage from "./pages/Securedpage";
+import PrivateRoute from "./helpers/PrivateRoute";
 
 function App() {
- return (
-   <div>
-     <Nav />
-     <BrowserRouter>
-       <Routes>
-         <Route exact path="/" element={<WelcomePage />} />
-         <Route path="/secured" element={<SecuredPage />} />
-       </Routes>
-     </BrowserRouter>
-   </div>
- );
+  return (
+    <div>
+      <ReactKeycloakProvider authClient={keycloak}>
+        <Nav />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<WelcomePage />} />
+            <Route
+              path="/secured"
+              element={
+                <PrivateRoute>
+                  <SecuredPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ReactKeycloakProvider>
+    </div>
+  );
 }
 
 export default App;
